9a0299a9c5abf0833affba2e75759e5c5d0b8dea
Juan Jose Aguilarte <barelrincon@gmail.com>
2022-08-09 20:47:02 +0200

    adsd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
-const sharp = require("sharp");
+//const sharp = require("sharp");
 const path = require("path");
 
 require("dotenv").config();
